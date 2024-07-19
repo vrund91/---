7ac63e4a170daef30700d0c65bd3649292c0fe69
Vrund Patel <vrundpatel876@gmail.com>
2024-07-19 16:36:25 +0530

    Update include.php

--- include.php
+++ include.php
@@ -1,9 +1,1 @@
-<html>
-<body>
-	<?php
-    include 'C:\xampp\htdocs\php-pr\include.php';
-	//require 'C:\xampp\htdocs\php-pr\include.php';
-	echo "rest part of the site<br>";
-	?>
-</body>
-</html>
+
